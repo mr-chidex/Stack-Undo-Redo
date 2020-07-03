dcbe37de57af0a92de9b87f9be9c5f205877bd36
CHIDEX <mbahchiboy2@gmail.com>
2020-07-03 08:30:56 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace UndoRedoSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.UndoBotton = new System.Windows.Forms.Button();
+            this.RedoBotton = new System.Windows.Forms.Button();
+            this.SaveBotton = new System.Windows.Forms.Button();
+            this.LoadSaved = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(37, 33);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(193, 20);
+            this.textBox.TabIndex = 0;
+            // 
+            // UndoBotton
+            // 
+            this.UndoBotton.Location = new System.Drawing.Point(37, 108);
+            this.UndoBotton.Name = "UndoBotton";
+            this.UndoBotton.Size = new System.Drawing.Size(75, 32);
+            this.UndoBotton.TabIndex = 1;
+            this.UndoBotton.Text = "Undo";
+            this.UndoBotton.UseVisualStyleBackColor = true;
+            this.UndoBotton.Click += new System.EventHandler(this.UndoBotton_Click);
+            // 
+            // RedoBotton
+            // 
+            this.RedoBotton.Location = new System.Drawing.Point(155, 108);
+            this.RedoBotton.Name = "RedoBotton";
+            this.RedoBotton.Size = new System.Drawing.Size(75, 32);
+            this.RedoBotton.TabIndex = 2;
+            this.RedoBotton.Text = "Redo";
+            this.RedoBotton.UseVisualStyleBackColor = true;
+            this.RedoBotton.Click += new System.EventHandler(this.RedoBotton_Click);
+            // 
+            // SaveBotton
+            // 
+            this.SaveBotton.Location = new System.Drawing.Point(99, 163);
+            this.SaveBotton.Name = "SaveBotton";
+            this.SaveBotton.Size = new System.Drawing.Size(75, 32);
+            this.SaveBotton.TabIndex = 4;
+            this.SaveBotton.Text = "Save";
+            this.SaveBotton.UseVisualStyleBackColor = true;
+            this.SaveBotton.Click += new System.EventHandler(this.SaveBotton_Click);
+            // 
+            // LoadSaved
+            // 
+            this.LoadSaved.Location = new System.Drawing.Point(99, 217);
+            this.LoadSaved.Name = "LoadSaved";
+            this.LoadSaved.Size = new System.Drawing.Size(75, 32);
+            this.LoadSaved.TabIndex = 5;
+            this.LoadSaved.Text = "Load Saved";
+            this.LoadSaved.UseVisualStyleBackColor = true;
+            this.LoadSaved.Click += new System.EventHandler(this.LoadSaved_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(255, 305);
+            this.Controls.Add(this.LoadSaved);
+            this.Controls.Add(this.SaveBotton);
+            this.Controls.Add(this.RedoBotton);
+            this.Controls.Add(this.UndoBotton);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button UndoBotton;
+        private System.Windows.Forms.Button RedoBotton;
+        private System.Windows.Forms.Button SaveBotton;
+        private System.Windows.Forms.Button LoadSaved;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UndoRedoSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stack<string> textContent = new Stack<string>();
+
+        Stack<string> allText = new Stack<string>();
+
+        private void UndoBotton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Catch last item ino stack
+            int lastItemIndex = textBox.Text.Length - 1;
+            string lastItem = textBox.Text.Substring(lastItemIndex);
+            textContent.Push(lastItem);
+
+            //Remove last item from textBox
+            textBox.Text = textBox.Text.Substring(0, lastItemIndex);
+            }
+            catch (ArgumentOutOfRangeException) { }
+           
+            
+            
+        }
+
+        private void RedoBotton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                textBox.Text += textContent.Pop();
+            }
+            catch (InvalidOperationException) { }
+          
+            
+        }
+
+        private void SaveBotton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string all = textBox.Text.Substring(0, textBox.Text.Length);
+                allText.Push(all);
+                textBox.Clear();
+            }
+            catch (ArgumentOutOfRangeException) { }
+            
+        }
+
+        private void LoadSaved_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox.Text = allText.Pop();
+            }
+            catch (InvalidOperationException) { }
+            
+        }
+    }
+}
